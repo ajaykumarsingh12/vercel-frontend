b7c280a9bfccfb6bc2cb03bfb715ebf97e40e155
Ajay Singh <133636514+ajaykumarsingh12@users.noreply.github.com>
2026-02-25 00:53:10 +0530

    slot-user-change

--- src/pages/HallDetail.jsx
+++ src/pages/HallDetail.jsx
@@ -934,11 +934,6 @@ const HallDetail = () => {
                               
                               {slot.isBooked && slot.user && (
                                 <div className="slot-booking-details">
-                                  <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                                    <circle cx="12" cy="7" r="4"></circle>
-                                  </svg>
-                                  <span>{slot.user.name} Booked</span>
                                 </div>
                               )}
 
