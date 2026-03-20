bee1bbaf93b4b51d77267e07a8dc802dcddc4ea0
Ajay Singh <133636514+ajaykumarsingh12@users.noreply.github.com>
2026-03-21 00:14:53 +0530

    modifychangescssstyle

--- src/pages/hallowner/MyHalls.jsx
+++ src/pages/hallowner/MyHalls.jsx
@@ -93,38 +93,10 @@ const MyHallCard = ({ hall, handleDelete }) => {
           )}
 
           <div className="image-label">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="14"
-              height="14"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M14.828 14.828a4 4 0 0 1-5.656 0"></path>
-              <path d="M9 10h1.586a1 1 0 0 1 .707.293l.707.707A1 1 0 0 0 13.414 11H15a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2H9a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2z"></path>
-            </svg>
             {currentImageIndex === 0 ? "Main Photo" : `Photo ${currentImageIndex + 1}`}
           </div>
           {images.length > 1 && (
             <div className="photo-count">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="12"
-                height="12"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M14.828 14.828a4 4 0 0 1-5.656 0"></path>
-                <path d="M9 10h1.586a1 1 0 0 1 .707.293l.707.707A1 1 0 0 0 13.414 11H15a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2H9a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2z"></path>
-              </svg>
               {images.length} photos
             </div>
           )}
